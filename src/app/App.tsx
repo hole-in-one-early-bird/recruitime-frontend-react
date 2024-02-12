aba15e11573386c43cb856c266ce9099ff66cb4c
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-02-12 10:18:11 +0900

    feat:react query setting

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SigninPage } from 'pages/signin';
 import { SignupPage } from 'pages/signup';
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router';
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </>
+    </QueryClientProvider>
   );
 }
 
