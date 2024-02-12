499dbe050bf0dc7e85d262fd34b45e8ef7d9f1d1
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-02-13 00:11:45 +0900

    #2 fix:css모듈 import 안되는 문제, :root로 형식 변경

--- src/pages/signin/ui/Page.tsx
+++ src/pages/signin/ui/Page.tsx
@@ -1,9 +1,11 @@
 import { SigninForm } from 'features/auth';
 import React from 'react';
+import { Typography } from 'shared/ui/typography/Typography';
 
 export const SigninPage = () => {
   return (
     <div>
+      <Typography variant={'count1'} children={'test'} />
       <SigninForm />
     </div>
   );
