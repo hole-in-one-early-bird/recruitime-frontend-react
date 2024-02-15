c532a175d5361714bdbacde677e17330f6d430bb
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-02-15 23:42:54 +0900

    #2 refactor:buttonType 파일 분리

---
+++ src/shared/ui/button/buttonType.ts
@@ -0,0 +1,44 @@
+import colors from 'shared/styles/color';
+import { css } from 'styled-components';
+
+const primaryCommonStyles = css`
+  padding: 18px;
+  border-radius: 10px;
+`;
+
+export const variantStyles = {
+  // 기본 버튼
+  primary: css`
+    ${primaryCommonStyles}
+    background-color: ${colors.blue[500]};
+    color: ${colors.white};
+  `,
+  // 입력 버튼 - inactive
+  confirm: css`
+    ${primaryCommonStyles}
+    background-color: ${colors.blue[400]};
+    color: ${colors.white};
+  `,
+  // 활성화 버튼
+  active: css`
+    ${primaryCommonStyles}
+    background-color: ${colors.blue[100]};
+    border: 1px solid ${colors.blue[400]};
+    color: ${colors.blue[600]};
+  `,
+  inactive: css`
+    ${primaryCommonStyles}
+    background-color: ${colors.gray[50]};
+    color: ${colors.gray[500]};
+  `,
+  primaryDisabled: css`
+    ${primaryCommonStyles}
+    background-color: ${colors.gray[200]};
+    color: ${colors.gray[400]};
+  `,
+  cancel: css`
+    ${primaryCommonStyles}
+    border: 1px solid #E0E0E0;
+    color: #838383;
+  `,
+};
