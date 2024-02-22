1318091f0684ddfe262ccffe6314eac2336a3076
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-02-22 12:20:34 +0900

    #3 feat: 모달 open시 검은 배경, 스크롤 방지

--- src/shared/ui/modal/Modal.tsx
+++ src/shared/ui/modal/Modal.tsx
@@ -17,30 +17,48 @@ interface OptionProps {
   isSelected: boolean;
 }
 
-export const Modal: React.FC<ModalProps> = ({ isOpen, onSelect, selected, options, label }) => {
+export const Modal: React.FC<ModalProps> = ({ isOpen, onSelect, selected, options, label, onClose }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }, [isOpen]);
   if (!isOpen) return null;
-
   const modalContent = (
-    <ModalWrapper>
-      {label && (
-        <Label>
-          <Typography variant={'label'}>{label}</Typography>
-        </Label>
-      )}
-      <OptionBox>
-        {options &&
-          onSelect &&
-          options.map((option) => (
-            <Option key={option} onClick={() => onSelect(option)} isSelected={selected === option}>
-              <Typography variant={'selectList'}>{option}</Typography>
-            </Option>
-          ))}
-      </OptionBox>
-    </ModalWrapper>
+    <Overlay onClick={onClose}>
+      <ModalWrapper>
+        {label && (
+          <Label>
+            <Typography variant={'label'}>{label}</Typography>
+          </Label>
+        )}
+        <OptionBox>
+          {options &&
+            onSelect &&
+            options.map((option) => (
+              <Option key={option} onClick={() => onSelect(option)} isSelected={selected === option}>
+                <Typography variant={'selectList'}>{option}</Typography>
+              </Option>
+            ))}
+        </OptionBox>
+      </ModalWrapper>
+    </Overlay>
   );
   return ReactDOM.createPortal(modalContent, document.body);
 };
-
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const ModalWrapper = styled.div`
   position: fixed;
   bottom: 0;
